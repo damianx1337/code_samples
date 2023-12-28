ed4968822982a75e2827c1e21ce23a773e6efd49
Dennis Sobczak <dennissobczak@Denniss-MBP.fritz.box>
2023-12-28 16:45:52 +0100

    adding to json object and restructuring json object

---
+++ golang/data_structures/src/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"encoding/json"
+)
+
+type Example struct {
+    Id []int
+    Name []string
+}
+
+func (data *Example) AppendOffer(id int, name string) {
+    data.Id = append(data.Id, id)
+    data.Name = append(data.Name, name)
+}
+
+var MyMap map[string]*Example
+
+func main() {
+    obj := &Example{[]int{}, []string{}}
+    obj.AppendOffer(1, "SomeText")
+    MyMap = make(map[string]*Example)
+    MyMap["key1"] = obj
+    MyMap["key1"].Name = append(MyMap["key1"].Name, "vlavla")
+    fmt.Println(MyMap["key1"].Name)
+    fmt.Println(reflect.TypeOf(MyMap["key1"]))
+
+		j, err := json.Marshal(MyMap)
+    if err != nil {
+			fmt.Printf("Error: %s", err.Error())
+		} else {
+			fmt.Println(string(j))
+		}
+}
