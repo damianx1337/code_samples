1e4fe9a79bd4b72d9c80cd957939595cbf9fbd49
Dennis Sobczak <dn.sobc@gmail.com>
2025-03-14 08:32:23 +0100

    added java api-gateway app

---
+++ java/api-gateway/src/main/java/com/example/apigateway/filters/GlobalRequestFilter.java
@@ -0,0 +1,29 @@
+package com.example.apigateway.filters;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalRequestFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        //log.info("Incoming request: {} {}", request.getMethod(), request.getURI());
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
+
